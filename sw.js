fd816b63a6c7f179fad90f7f1b43580e741dfc14
Nachopiedra <nachopiedra@gmail.com>
2026-04-15 09:01:17 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,34 @@
-const CACHE_NAME = 'radio-v2';
+const CACHE_NAME = 'radio-v3';
 const assets = [
   './',
   './index.html',
   './icono.png',
   './manifest.json'
 ];
-
+ 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
   );
 });
-
+ 
+self.addEventListener('activate', event => {
+  // Borrar cachés antiguas al activar la nueva versión
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+ 
 self.addEventListener('fetch', event => {
   event.respondWith(
     fetch(event.request).catch(() => caches.match(event.request))
   );
 });
+ 
+// Permite activación inmediata cuando el index.html lo solicita
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
+});
+ 
